aa2d80314a78ab38418cb1ce7e5e5e70c2b1322b
UGEcko <vanhelsing314@outlook.com>
2022-09-20 20:49:47 -0400

    Create Circle.ts

---
+++ Scripts/Circle.ts
@@ -0,0 +1,21 @@
+
+export function Circ(trackN: string, startBeat: number, dur: number, radius: number, amount: number, h: number, l: number, xPos: number, yPos: number, zPos: number, rot: number, disTime: number) {
+    for (let i = 0; i <amount; i++) {
+        let angle = Math.PI*2 / amount
+        let rot = 360/amount*i
+        let radians = angle *i
+        let w  = 2*radius*Math.tan(Math.PI/amount)
+        let sx = xPos + Math.cos(radians) * radius-w/2
+        let sy = yPos + Math.sin(radians) * radius-h/2
+        const wall = new Wall(startBeat,dur,0,0,0)
+        wall.time = (startBeat+2)+0.005*i
+        wall.interactable = false
+        wall.track.value = trackN
+        wall.scale = [w,h,l]
+        wall.rotation = [0,0,0]
+        wall.localRotation = [0,0,90+rot]
+        wall.position = [sx,sy,0]
+        wall.animate.definitePosition = [0,0,zPos]
+        wall.color = [1,1,1,1]
+    }
+}
